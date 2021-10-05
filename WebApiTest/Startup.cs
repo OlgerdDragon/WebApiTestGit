7cc85f67c26f3c6f3f63309b8d0278cf66301520
Vlad Bilokon <olgerddragon@gmail.com>
2021-10-05 13:49:45 +0300

    test jwt

--- WebApiTest/Startup.cs
+++ WebApiTest/Startup.cs
@@ -49,14 +49,16 @@ namespace WebApiTest
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiTest v1"));
             }
 
             app.UseHttpsRedirection();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
+            
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
